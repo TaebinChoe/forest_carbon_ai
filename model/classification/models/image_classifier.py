cfa2b8c2c1e61a9bc5f94d03a9fb6fcbfc2602a4
TaebinChoe <taebbb99@gmail.com>
2025-02-08 22:00:23 +0900

    0208

--- model/classification/models/image_classifier.py
+++ model/classification/models/image_classifier.py
@@ -152,6 +152,81 @@ class ResNetLike_60_3(nn.Module):
 
 """
 <input_shape>
+channel: 108, patch_size:3
+"""
+class BasicBlock_108_3(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock_108_3, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        self.downsample = None
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+        
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += identity
+        out = self.relu(out)
+        return out
+
+class ResNetLike_108_3(nn.Module):
+    def __init__(self, num_classes=6):
+        super(ResNetLike_108_3, self).__init__()
+        self.in_channels = 128  # 초기 채널 수 설정
+        
+        # 108 채널 입력을 처리할 Conv Layer
+        self.conv1 = nn.Conv2d(108, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.relu = nn.ReLU(inplace=True)
+        
+        # 블록 채널 증가: 128 → 256 → 512
+        self.layer1 = self._make_layer(128, 2, stride=1)
+        self.layer2 = self._make_layer(256, 2, stride=1)
+        self.layer3 = self._make_layer(512, 2, stride=1)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+    
+    def _make_layer(self, out_channels, blocks, stride):
+        layers = []
+        layers.append(BasicBlock_108_3(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels
+        for _ in range(1, blocks):
+            layers.append(BasicBlock_108_3(out_channels, out_channels))
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+    
+    
+"""
+<input_shape>
 channel: 120, patch_size:3
 """
 class BasicBlock_120_3(nn.Module):
@@ -223,7 +298,7 @@ class ResNetLike_120_3(nn.Module):
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
-
+    
 
 """
 <input_shape>
